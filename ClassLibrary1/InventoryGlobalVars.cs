5cc66b32da7a5dcc434cfae8abc0aea554bb1b51
rommaat0180 <faryadali01@gmail.com>
2017-02-07 12:17:12 +0500

    ddd

---
+++ ClassLibrary1/InventoryGlobalVars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ClassLibrary1
+{
+    public class InventoryGlobalVars
+    {
+       SqlConnection cn = new SqlConnection(@"Connection string");
+    }
+}
